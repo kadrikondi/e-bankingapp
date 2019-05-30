319ff5de1f269d1d1f95394acb1b30e8555f5984
kadrikondi <abdulkadri42@gmail.com>
2019-05-30 11:29:00 +0100

    app production confiq

--- server/app.js
+++ server/app.js
@@ -1,19 +1,52 @@
 const app = require('express')()
 const bodyParser = require('body-parser')
 const mongoose = require('mongoose')
+// const passport = require('passport')
 const routes = require('./routes/routes')
+const path = require('path')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended:true}))
 app.use('/', routes)
 
-const port = 8000
+const port = process.env.PORT || 9000;
 
-mongoose.connect('mongodb://localhost:27017/EBanking', { useNewUrlParser: true })
-.then( ()=>{
-    app.listen(port, () => {
-        console.log(`e-Banking is using ${port}`)
-    }) 
+
+app.listen(port, () => {
+    if (process.env.NODE_ENV === 'production') {
+        mongoose.connect('mongodb://eventmanager:kadzee222@ds231740.mlab.com:31740/kondipressdb', {
+                useNewUrlParser: true
+            })
+            .then(() => {
+
+                console.log("mongodb connected online")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+    } else {
+        mongoose.connect('mongodb://localhost:27017/EBanking', {
+                useNewUrlParser: true
+            })
+            .then(() => {
+
+                console.log("mongodb connected offline")
+            })
+            .catch((err) => {
+                console.log(err)
+            })
+
+    }
+    console.log(`our app is listening on port ${port}`)
 })
-.catch( err => console.log(err))
+
+
+// // befor
+// mongoose.connect('mongodb://localhost:27017/EBanking', { useNewUrlParser: true })
+// .then( ()=>{
+//     app.listen(port, () => {
+//         console.log(`e-Banking is using ${port}`)
+//     }) 
+// })
+// .catch( err => console.log(err))
 
 // this is cooladd
