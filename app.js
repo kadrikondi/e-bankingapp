8a0bfefd9b71a5b21cd3684a1d0c38ae5c3f7e80
kadrikondi <abdulkadri42@gmail.com>
2019-05-30 17:20:10 +0100

    pushing to heroku

--- app.js
+++ app.js
@@ -20,8 +20,8 @@ if (process.env.NODE_ENV === 'production') {
 
 
     app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'build', 'index.html'))
-        // res.send(`<h1>ERORR 404, PAGE NOT FOUND</h1>`)
+        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+        res.send(`<h1>ERORR 404, PAGE NOT FOUND</h1>`)
     })
 }
 
