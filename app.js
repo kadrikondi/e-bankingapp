ef0641c94ead509137300efde77c4564e3a21c84
otitoju <otitojuoluwapelumi@gmail.com>
2019-07-01 12:30:32 +0100

    scale server

--- app.js
+++ app.js
@@ -1,77 +1,86 @@
-// const app = require('express')()
-const express = require('express')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
+const os = require('os')
+const cluster = require('cluster')
 
-// const passport = require('passport')
-const app = express();
-const routes = require('./routes/routes')
-const path = require('path')
-app.use(bodyParser.json())
-app.use(bodyParser.urlencoded({extended:true}))
-app.use('/', routes)
+if(cluster.isMaster){
+    //cluster master
+    const n_cpus = os.cpus().length
+    console.log(`Forking ${n_cpus} CPUS`)
+    console.log(`Master is using ${process.pid} process`)
+    for(let i=0; i < n_cpus; i++){
+        cluster.fork()
+    }
+}
+else {
+    //cluster worker
+    const express = require('express')
+    const bodyParser = require('body-parser')
+    const mongoose = require('mongoose')
+    const morgan = require('morgan')
+    const app = express();
+    const routes = require('./routes/routes')
+    const path = require('path')
+    const pid = process.pid
+    app.use(morgan('dev')) // install morgan
+    app.use(bodyParser.json())
+    app.use(bodyParser.urlencoded({extended:true}))
+    app.use('/', routes)
 
-if (process.env.NODE_ENV === 'production') {
+    if (process.env.NODE_ENV === 'production') {
+        app.use(express.static(path.resolve(__dirname, 'client/build')))
+        app.use(express.static(path.resolve(__dirname, 'build', 'index.html')))
+
+        app.get('*', (req, res) => {
+            for(let i = 0; i < 2e6; i++){
+
+            }
+            console.log(`Process ${pid} is here`)
+            res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
+        })
+    }
+    app.use((req, res, next) => {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+        res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+        res.header("x-forward-for")
+        next()
+      })
+    const port = process.env.PORT || 9000;
 
 
-    app.use(express.static(path.resolve(__dirname, 'client/build')))
-    app.use(express.static(path.resolve(__dirname, 'build', 'index.html')))
-    console.log()
+    app.listen(port, () => {
+        if (process.env.NODE_ENV === 'production') {
+            mongoose.connect('mongodb+srv://yakubebank:kadzee222.@cluster0-opegc.mongodb.net/test?retryWrites=true&w=majority', {
+                    useNewUrlParser: true
+                })
+                .then(() => {
 
+                    console.log(`Yakub e-banking server is using ${pid} process running on ${port}`)
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+        } else {
+            mongoose.connect('mongodb://localhost:27017/EBanking', {
+                    useNewUrlParser: true
+                })
+                .then(() => {
 
-    app.get('*', (req, res) => {
-        res.sendFile(path.resolve(__dirname, 'client/build/index.html'))
-        // res.send(`<h1>ERORR 404, PAGE NOT FOUND</h1>`)
+                    console.log("mongodb connected offline")
+                })
+                .catch((err) => {
+                    console.log(err)
+                })
+
+        }
+        console.log(`Yakub e-banking server is using ${pid} process running on ${port}`)
+    })
+    cluster.on('exit', (worker) => {
+        console.log(`Alert: worker, ${worker.id} is no more`)
+        cluster.fork()
     })
 }
 
-const port = process.env.PORT || 9000;
-
-
-app.listen(port, () => {
-    if (process.env.NODE_ENV === 'production') {
-        mongoose.connect('mongodb+srv://yakubebank:kadzee222.@cluster0-opegc.mongodb.net/test?retryWrites=true&w=majority', {
-                useNewUrlParser: true
-            })
-            .then(() => {
-
-                console.log("mongodb connected online")
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-    } else {
-        mongoose.connect('mongodb://localhost:27017/EBanking', {
-                useNewUrlParser: true
-            })
-            .then(() => {
-
-                console.log("mongodb connected offline")
-            })
-            .catch((err) => {
-                console.log(err)
-            })
-
-    }
-    console.log(`our app is listening on port ${port}`)
+cluster.on('exit', (worker) => {
+    console.log(`Alert: worker, ${worker.id} is no more`)
+    cluster.fork()
 })
-
-
-// // befor
-// mongoose.connect('mongodb://localhost:27017/EBanking', { useNewUrlParser: true })
-// .then( ()=>{
-//     app.listen(port, () => {
-//         console.log(`e-Banking is using ${port}`)
-//     }) 
-// })
-// .catch( err => console.log(err))
-
-// this is cooladd
-
-
-
-// 
-//   we are happy here 
-
-
-// this is the one we use in production
